7c4f4b0dcf46c1e777d2dd674249802b5c259060
Oriol <oriolmilajansa@gmail.com>
2018-11-30 12:35:52 +0100

    Create basic functionality

--- app.js
+++ app.js
@@ -1,7 +1,47 @@
-var standingsApp = angular.module("standingsApp", []);
+var standingsApp = angular.module("standingsApp", ["ngRoute"]);
 
-standingsApp.config()
+// Routes
+standingsApp.config(function($routeProvider){
+	$routeProvider
+	.when("/", {
+		templateUrl: "views/standings.html",
+		controller: 
+	})
+	.when("/teams", {
+		templateUrl: "views/teams.html",
+		controller:
+	})
+	.otherwise({
+		redirectTo("/")
+	}) 
+});
 
-standingsApp.controller("StandingsController", function($scope){
-	$scope.teams
-})
+// Controller
+standingsApp.controller("StandingsController", ["$scope", "$http", function($scope, $http){
+	
+	$scope.addTeam = function(){
+		$scope.teams.push({
+			name = $scope.newTeam.name;
+			cups = parseInt($scope.newTeam.cups);
+			leagues = parseInt($scope.newTeam.leagues);
+			champions = parseInt($scope.newTeam.champions);									
+		})
+
+		$scope.newTeam.name = "";
+		$scope.newTeam.leagues = "";
+		$scope.newTeam.champions = "";
+
+	}
+
+	$scope.removeTeam = function(team){
+		var removedTeam = indexOf(team);
+		$scope.teams.splice(removedTeam, 1);
+	}
+
+	// Get data
+	$http.get("teams.json").then(function(data){
+		$scope.teams = data.data;
+	});
+
+}]);
+
